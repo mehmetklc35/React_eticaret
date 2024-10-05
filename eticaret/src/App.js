b2abbb4ba3f779331ab1474023a3e30110b00e64
Your Name <you@example.com>
2024-10-05 10:30:39 +0300

    sayfalar oluşturuldu tailwindde hata var düzeltilecek...

--- eticaret/src/App.js
+++ eticaret/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import './index.css';
+import Layout from './components/layout';
+import Home from './pages/home';
+import Detail from './pages/detail';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='/:slug' element={<Detail />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ eticaret/src/products.js
@@ -0,0 +1,74 @@
+import imageProduct1 from './assets/image/1.png' 
+import imageProduct2 from './assets/image/2.png' 
+import imageProduct3 from './assets/image/3.png' 
+import imageProduct4 from './assets/image/4.png' 
+import imageProduct5 from './assets/image/5.png' 
+import imageProduct6 from './assets/image/6.png' 
+import imageProduct7 from './assets/image/7.png' 
+import imageProduct8 from './assets/image/8.png' 
+export const products = [
+    {
+        id: 1,
+        name: 'Nike Go FlyEase',
+        price: 5.799,
+        image: imageProduct1,
+        description: 'Dışarı çıkmadan önce bağcıkları bağlamaya elveda de. Nikeın çığır açan FlyEase teknolojisine sahip bu ayakkabıyı kolayca giyip çıkarabilirsin. Ellerini kullanmadan ayakkabını giymen için dönerek açılan bir topuğa sahip olan bu stil, hem hareket kabiliyeti kısıtlı kişiler hem de hemen harekete geçmek isteyenler için idealdir.',
+        slug: 'Nike Go FlyEase'
+    },
+    {
+        id: 2,
+        name: 'Air Jordan 1 Low',
+        price: 5.299,
+        image: imageProduct2,
+        description: 'İlk kez 1985te satışa sunulan orijinal modelden ilham alan Air Jordan 1 Low, tanıdık görünen fakat her zaman yeni kalmayı başaran, sade ve klasik bir stil sunar. Her stile mükemmel uyum sağlayan ikonik tasarımıyla bu ayakkabı, her zaman şık olmanı sağlar.',
+        slug: 'Air Jordan 1 Low'
+    },
+    {
+        id: 3,
+        name: 'Air Jordan 1 Mid',
+        price: 5.499,
+        image: imageProduct3,
+        description: 'Orijinal AJ1dan ilham alan bu normal bilekli tasarım sevdiğin ikonik stili korurken farklı renkler ve kaliteli deri, tasarıma benzersiz bir kimlik kazandırır.',
+        slug: 'Air Jordan 1 Mid'
+    },
+    {
+        id: 4,
+        name: 'Jordan Stay Loyal 3',
+        price: 2.699,
+        image: imageProduct4,
+        description: 'Geleceğini tasarlayabilmen için geçmişini bilmen gerekir. Air Jordan mirasını temel alan modern bir ayakkabı olan Stay Loyal 3 modelini tasarlarken bu sözden ilham aldık. Her ayrıntısıyla çok yönlülüğe odaklanan bu stil, minimalist görünüme, bulutları andıran yastıklamaya ve AJ4ten esintiler taşıyan tasarım öğelerine sahiptir. Kısacası uzun süre şık görünmeni sağlayan dayanıklı bir stil sunar.',
+        slug: 'Jordan Stay Loyal 3'
+    },
+    {
+        id: 5,
+        name: 'Nike Air Max Plus Drift',
+        price: 7.799,
+        image: imageProduct5,
+        description: 'Üstün destek ve yastıklamayla "tuned" Air deneyimi sunan Air Max Plus Drift ile tavrını ortaya koy. Havadar fileye, geçişli renklere ve orijinal modelin dalgalı tasarım çizgilerine sahip olan bu ayakkabı, asi stilini yansıtır.',
+        slug: 'Nike Air Max Plus Drift'
+    },
+    {
+        id: 6,
+        name: 'Nike Dunk Low Retro SE',
+        price: 5.799,
+        image: imageProduct6,
+        description: 'Klasik stillere her zaman güvenebilirsin. İkonik renk blokları ve dayanıklı malzemeleri yumuşak dolguyla birleştiren Dunk Low, uzun süreli olağanüstü rahatlık sunar. Olasılıklar sınırsız. Dunkını nasıl giyeceksin?',
+        slug: 'Nike Dunk Low Retro SE'
+    },
+    {
+        id: 7,
+        name: 'Nike Air Max Plus 3',
+        price: 3.999,
+        image: imageProduct7,
+        description: 'Kendinden önceki modellerin ikonik Özel Basınç Ayarlı Air teknolojisine sahip Nike Air Max Plus 3, kadifemsi sentetik deri üst kısmına eklenmiş TPU ile stili yeniliyor. OG modele saygı duruşunda bulunan bu stil, keskin bir kontrast oluşturan renklerle tüm dikkatleri üstüne çekerek üstünlük elde etmeni sağlar.',
+        slug: 'Nike Air Max Plus 3'
+    },
+    {
+        id: 8,
+        name: 'Jordan One Take 5',
+        price: 2.799,
+        image: imageProduct8,
+        description: 'Hızlan, potaya koş, atış yap, sayı kazan ve tekrarla. Russell Westbrookun en yeni ayakkabısıyla oyununa hız kat ve durdurulamaz ol. Dış bağcık gözü ve ayağı sıkıca saran burun kısmı sahada koruma sunar. Ayak tarağındaki Zoom Air yastıklama ise oyun boyunca en iyi performansını göstermen için enerji kazanımı sağlar.',
+        slug: 'Jordan One Take 5'
+    },
+];
